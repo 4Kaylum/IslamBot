63a11fc7c514622fdc90d237706b20695f40b37b
Callum Bartlett <coolum444@hotmail.co.uk>
2017-06-25 18:44:52 +0100

    Buyrole
    The buyrole command is now added to the bot. You can purchase a role, as
    determined in the "buyableRoles" json file, if you have the right amount
    of money to be able to do it.

--- Cogs/UserMoney.py
+++ Cogs/UserMoney.py
@@ -14,8 +14,51 @@ class UserMoney(object):
         Shows you how much money you have in your user account
         '''
 
-        userData = getFileJson('money.json')
+        userData = getFileJson('userMoney.json')
         userMoney = userData.get(ctx.message.author.id, 0)
         await self.bot.whisper('You have `{}` credits in your account.'.format(userMoney))
 
     @commands.command(pass_context=True)
+    async def buyrole(self, ctx, *, roleName:str):
+        '''
+        Lets you buy a role
+        '''
+
+        roleData = getFileJson('buyableRoles.json')
+        roleIDs = list(roleData.keys())
+        serverRoles = list(ctx.message.roles)
+        availableRoles = [i for i in serverRoles if i.id in roleIDs]
+
+        # Determine what role they were trying to search for
+        wantedRole = [i for i in availableRoles if roleName.lower() in i.name.lower()]
+
+        # Determine if that role exists
+        if len(wantedRole) == 0:
+            await self.bot.say('There were no roles that matched the hitstring `{}`.'.format(roleName))
+            return
+        elif len(wantedRole) > 1:
+            await self.bot.say('There were multiple roles that matched the hitstring `{}`; \n* {}'.format(roleName, .'\n* '.join(wantedRole)))
+            return
+        else:
+            # There was only one role that matched that hitstring
+            pass
+
+        # Determine whether or not they have enough money for it
+        userData = getFileJson('userMoney.json')
+        userMoney = userData.get(ctx.message.author.id, 0)
+        roleCost = roleData.get(str(wantedRole[0].id))
+        if userMoney < roleCost:
+            await self.bot.say('You don\'t have enough money to purchase that role.')
+            return
+
+        # They have enough money - add the role and deduct money from their account
+        self.bot.add_roles(ctx.message.author, wantedRole[0])
+        userMoney -= roleCost
+        userData[ctx.message.author.id] = userMoney
+        saveFileJson('userMoney.json', userData)
+        await self.bot.say('The role `{}` has been sucessfully added to you for `{}` credits.'.format(wantedRole[0].name, roleCost))
+
+
+def setup(bot):
+    x = UserMoney(bot)
+    bot.add_cog(x)
